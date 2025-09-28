e9f988d85ac0000259e6f8bbc04df9b7f0f047f7
marto9696 <m.hristev@ubiquedigital.com>
2025-09-28 10:54:39 +0100

    incorporated postcode filtering

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m8c1eu0qvq8"
+    "revision": "0.337neoksrpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
