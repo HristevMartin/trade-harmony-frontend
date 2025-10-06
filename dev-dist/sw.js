b03a2dc94d1d9441e19f0c6ac1823aed39cc236d
marto9696 <m.hristev@ubiquedigital.com>
2025-10-06 16:43:13 +0100

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tmg4f4ss8mo"
+    "revision": "0.pdstk0vdq3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
